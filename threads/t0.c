1632fb3d507ae9aefd4a2131109ed38b7d60d946
AhmadAsif9839 <ahmadasif9839@gmail.com>
2022-11-15 21:36:16 +0500

    threading

--- threads/t0.c
+++ threads/t0.c
@@ -27,7 +27,7 @@
 // {
 //     f1();
 //     f2();
-//     printf("Bye Bye from main\n");
+//     printf("\nBye Bye from main\n");
 
 //     return 0;
 // }
@@ -47,18 +47,22 @@ void * f2(void *);
 int main()
 {
     pthread_t tid1, tid2;
+
+    // Create two child threads and wait for their termination
     pthread_create(&tid1, NULL, f1, NULL);
     pthread_create(&tid2, NULL, f2, NULL);
 
 
     // Both threads are executing concurrently
+
+    // Join the two threads
     pthread_join(tid1, NULL);
     pthread_join(tid2, NULL);
 
 
-    printf("Bye Bye from main thread\n");
+    printf("\nBye Bye from main thread\n");
 }
-
+ 
 void *f1(void * arg) {
     for (int i = 0; i < 5; i++) {
         printf("%s", "AHMAD");
